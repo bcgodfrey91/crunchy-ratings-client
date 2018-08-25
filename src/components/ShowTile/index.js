f86537b0e73936b43e0fae75d12eb400fbee2887
Ben Godfrey <bcgodfrey91@gmail.com>
2018-08-25 17:41:38 -0500

    complete mobile styling

--- src/components/ShowTile/index.js
+++ src/components/ShowTile/index.js
@@ -5,27 +5,27 @@ import star from './star.svg';
 const ShowTile = ({ title, description, image, rating, href }) => {
   return (
     <div className='show-tile'>
-      <div className="image-container">
-        <img
-          className="show-image"
-          src={image}
-          alt={`${title} Poster`}
-        />
-      </div>
       <div className="show-tile-content-container">
-        <div className="show-tile-content">
-          <h3 className="show-title">{title}</h3>
-          <p className="show-description">{description}</p>
+        <div className="image-container">
+          <img
+            className="show-image"
+            src={image}
+            alt={`${title} Poster`}
+          />
         </div>
-        <div className="rating-container">
-          <div className="rating-border">
-            <img
-              className="star-icon"
-              src={star}
-              alt="Star Rating Icon"
-            />
-            <h3 className="show-rating">{rating}</h3>
+          <div className="show-tile-content">
+            <p className="show-title">{title}</p>
+            <p className="show-description">{description}</p>
           </div>
+          <div className="rating-container">
+            <div className="rating-border">
+              <img
+                className="star-icon"
+                src={star}
+                alt="Star Rating Icon"
+              />
+              <p className="show-rating">{rating}</p>
+            </div>
             <a
               className="show-link"
               href={href}
